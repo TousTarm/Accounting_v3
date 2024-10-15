50af4bcde83c75929ce07b0e05fdd2f46144c9b2
TousTarm <tomas.rindt@gmail.com>
2024-10-15 18:10:01 +0200

    10-15-broken

--- app.py
+++ app.py
@@ -1,44 +1,50 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client["accounting"]        
+db = client["accounting"]
 
 def sum(data):
-    sum = 0
-    with open('keywords.json', 'r', encoding='utf-8') as f: #NACTENI KEYWORDS
-        keywords = json.load(f)
-        sums_by_type = {v[0]: 0 for k, v in keywords.items()}
+    sum_total = 0
+    # Load keywords from MongoDB 'keywords' collection
+    keywords_cursor = db["keywords"].find()
+    sums_by_type = {keyword["type"]: 0 for keyword in keywords_cursor}
+
     for row in data:
-        sum += int(float(row['amount']))
+        sum_total += int(float(row['amount']))
         amount = int(float(row['amount']))
         type_ = row['type']
-        for key, values in keywords.items():
-            if type_ == values[0]:
-                sums_by_type[values[0]] += amount
-    
-    ordered_sums = [sums_by_type[values[0]] for key, values in keywords.items()]
-    ordered_sums.insert(0,sum)
-    return ordered_sums
         
+        # Refresh the keywords cursor each time to check against new type
+        keywords_cursor = db["keywords"].find()
+        for keyword in keywords_cursor:
+            if type_ == keyword["type"]:
+                sums_by_type[keyword["type"]] += amount
+
+    # Recalculate the sums by type and create ordered sums
+    ordered_sums = list(sums_by_type.values())
+    ordered_sums.insert(0, sum_total)  # Insert the total sum at the start
+    return ordered_sums
+
 @app.route('/')
 def home():
     return render_template('index.j2')
 
 @app.route('/edit')
 def edit():
-    with open('keywords.json', 'r', encoding='utf-8') as f: #NACTENI KEYWORDS
-        keywords = json.load(f)
-        keywords = [[values[0], values[1]] for values in keywords.values()]
+    # Fetch keywords from MongoDB 'keywords' collection
+    keywords_cursor = db["keywords"].find()
+    keywords = [[keyword["type"], keyword["flag"]] for keyword in keywords_cursor]
+
+    # Filter data based on query parameters
     if request.args.get('parameter') is None:
         data = list(db['srpen'].find())
         totals = sum(data)
-        return render_template('edit.j2',data=data,totals=totals,keywords=keywords)
+        return render_template('edit.j2', data=data, totals=totals, keywords=keywords)
     else:
         if request.args.get('positive') and request.args.get('negative'):
             data = list(db['srpen'].find())
@@ -52,19 +58,19 @@ def edit():
         else:
             data = list(db['srpen'].find())
             totals = sum(data)
-        return render_template('edit.j2',data=data,totals=totals,keywords=keywords)
-    
+        return render_template('edit.j2', data=data, totals=totals, keywords=keywords)
+
 @app.route('/get_data/<string:object_id>', methods=['GET'])
 def get_data(object_id):
     try:
         document = db['srpen'].find_one({"_id": ObjectId(object_id)})
         if document is None:
             return jsonify({"error": "Document not found"}), 404
-        document['_id'] = str(document['_id'])
+        document['_id'] = str(document['_id'])  # Convert ObjectId to string
         return jsonify(document)
-    
+
     except Exception as e:
-        print(f"An error occurred: {e}, něco se posralo kurwa drat")
+        print(f"An error occurred: {e}")
         return jsonify({"error": "An unexpected error occurred"}), 500
 
 @app.route('/update_data', methods=['POST'])
@@ -78,7 +84,7 @@ def update_data():
     type_ = request.form.get('type')
     flag = request.form.get('flag')
 
-    print(date,amount,account,note,type_,flag)
+    print(date, amount, account, note, type_, flag)
 
     db['srpen'].update_one(
         {'_id': ObjectId(object_id)},
@@ -94,6 +100,15 @@ def update_data():
         }
     )
     return redirect(url_for('edit'))
+
+@app.route('/get_keywords', methods=['GET'])
+def get_keywords():
+    try:
+        keywords = list(db['keywords'].find({}, {"_id": 0}))  # Exclude _id from the result
+        return jsonify(keywords)  # Send the keywords as JSON
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/modal.js
+++ static/js/modal.js
@@ -1,5 +1,3 @@
-console.log("js loaded");
-
 document.addEventListener('DOMContentLoaded', function() {
     const typeFlagButtons = document.querySelectorAll('.type, .flag');
     const modal = document.getElementById('modal');
@@ -9,8 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
     typeFlagButtons.forEach(button => {
         button.addEventListener('click', function(event) {
             const objectId = event.target.value;
-            console.log("Button clicked with Object ID:", objectId);
-
             // Show modal and overlay
             modal.style.display = 'flex';
             modalOverlay.style.display = 'flex';
@@ -18,26 +14,27 @@ document.addEventListener('DOMContentLoaded', function() {
             // Fetch data from the server
             fetch(`/get_data/${objectId}`)
             .then(response => {
-                console.log("Response Status:", response.status); // Log response status
                 return response.text(); // Get raw response text
             })
             .then(rawText => {
-                console.log("Raw Response:", rawText); // Log raw response
                 return JSON.parse(rawText); // Parse the raw text as JSON
             })
             .then(data => {
                 modal.innerHTML = `
                     <form method="post" class="update_form" action="/update_data">
-                        <input type="hidden" name="id" value="${data._id}"/> <!-- Assuming data._id contains the ObjectId -->
+                        <input type="hidden" name="id" value="${data._id}"/>
                         <input type="text" name="date" class="field text-sm rounded-lg p-[4px]" placeholder="dd.mm.yyyy" value="${data.date}"/>
                         <input type="text" name="amount" class="field text-sm rounded-lg p-[4px]" placeholder="Amount" value="${data.amount}"/>
                         <input type="text" name="account" class="field text-sm rounded-lg p-[4px]" placeholder="Account" value="${data.account}"/>
                         <textarea name="note" class="field-note text-sm rounded-lg p-[4px]" placeholder="Note" rows="4">${data.note}</textarea>
-                        <input type="text" name="type" class="field text-sm rounded-lg p-[4px]" placeholder="Type" value="${data.type}"/>
-                        <input type="text" name="flag" class="field text-sm rounded-lg p-[4px]" placeholder="Flag" value="${data.flag}"/>
+                    
+                        <div class="dropdown-container">
+                            <div id="dropdown-btn type-btn" onclick="open_dropdown()">Select Type</button>
+                            <div id="dropdown-content" style="display: none;"></div>
+                        </div>
+
                         <button class="bg-white text-black w-[70px] h-[25px]" type="submit">Submit</button>
                     </form>
-
                     `;
             })
             .catch(error => console.error('Error fetching data:', error));
